fc8611e892d818cd7d3ef47635cd581f8c559e7c
Ranajayy <84616487+Ranajayy@users.noreply.github.com>
2026-05-20 16:12:04 +0500

    Delete ESP32 Projects directory

--- ESP32 Projects/cannabis_plant _growth_control_delivery_esp32_blynk/codes/plant_calibration/plant_calibration.ino
+++
@@ -1,7 +0,0 @@
-void setup() {
-Serial.begin(115200); // open serial port, set the baud rate to 115200 bps
-}
-void loop() {
-Serial.println(analogRead(15)); //connect sensor and print the value to serial
-delay(100);
-}
--- ESP32 Projects/oled file trasnmission delivery project/readme.txt
+++
@@ -1,2 +0,0 @@
-Create a menu for oled to send files from sd card over 
-rf using a module on esp32
